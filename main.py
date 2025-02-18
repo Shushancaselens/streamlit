18f9102ad98ad7ba2a62ea176871031dfd86ad40
Shushancaselens <shushan@caselens.tech>
2025-02-19 02:04:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -346,7 +346,6 @@ def create_position_section(position_data, position_type):
 def main():
     # Sidebar
     with st.sidebar:
-        # Logo and title
         st.markdown("""
         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 175 175">
           <mask id="whatsapp-mask" maskUnits="userSpaceOnUse">
@@ -464,13 +463,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-        st.markdown("""
-        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 175 175">
-          <mask id="whatsapp-mask" maskUnits="userSpaceOnUse">
-            <path d="M174.049 0.257812H0V174.258H174.049V0.257812Z" fill="white"/>
-          </mask>
-          <g mask="url(#whatsapp-mask)">
-            <path d="M136.753 0.257812H37.2963C16.6981 0.257812 0 16.9511 0 37.5435V136.972C0 157.564 16.6981 174.258 37.2963 174.258H136.753C157.351 174.258 174.049 157.564 174.049 136.972V37.5435C174.049 16.9511 157.351 0.257812 136.753 0.257812Z" fill="#4D68F9"/>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M137.367 54.0014C126.648 40.3105 110.721 32.5723 93.3045 32.5723C63.2347 32.5723 38.5239 57.1264 38.5239 87.0377C38.5239 96.9229 41.1859 106.155 45.837 114.103L45.6925 113.966L37.918 141.957L65.5411 133.731C73.8428 138.579 83.5458 141.355 93.8997 141.355C111.614 141.355 127.691 132.723 137.664 119.628L114.294 101.621C109.53 108.467 101.789 112.187 93.4531 112.187C79.4603 112.187 67.9982 100.877 67.9982 87.0377C67.9982 72.9005 79.6093 61.7396 93.751 61.7396C102.236 61.7396 109.679 65.9064 114.294 72.3052L137.367 54.0014Z" fill="white"/>
-          </g>
-        </svg>
